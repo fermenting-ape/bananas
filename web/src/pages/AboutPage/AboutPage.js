b0071bd164d77ddb375bf932360f6c4371b4acf3
mkhbragg <mk.bragg@pm.me>
2020-11-07 23:07:40 -0500

    :truck: About and Home pages added

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import MainLayout from 'src/layouts/MainLayout'
+
+const AboutPage = () => {
+  return (
+    <MainLayout>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
+    </MainLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import MainLayout from 'src/layouts/MainLayout'
+
+const HomePage = () => {
+  return <MainLayout>Home</MainLayout>
+}
+
+export default HomePage
